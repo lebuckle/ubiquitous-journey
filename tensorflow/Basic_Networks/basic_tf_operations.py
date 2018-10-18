8e5c9ba18095289b4c7cafd194a855c5f2ecc1f2
Léonie <leoniebuckley@gmail.com>
2018-10-18 09:31:47 +0100

    Updating python commands

--- tensorflow/Basic_Networks/basic_tf_operations.py
+++ tensorflow/Basic_Networks/basic_tf_operations.py
@@ -1,23 +1,29 @@
+'''
+Created on 9 Aug 2018
+
+@author: Leonie
+'''
+# https://leimao.github.io/article/Siamese-Network-MNIST/
 import tensorflow as tf
 import numpy as np
 # first, create a TensorFlow constant
 const = tf.constant(2.0, name="const")
-    
-# create TensorFlow variables
-# create TensorFlow variables
-b = tf.placeholder(tf.float32, [None, 1], name='b')
-c = tf.Variable(1.0, name='c')
+with tf.device('/cpu:0'):    
+	# create TensorFlow variables
+	# create TensorFlow variables
+	b = tf.placeholder(tf.float32, [None, 1], name='b')
+	c = tf.Variable(1.0, name='c')
 
-# now create some operations
-d = tf.add(b, c, name='d')
-e = tf.add(c, const, name='e')
-a = tf.multiply(d, e, name='a')
+	# now create some operations
+	d = tf.add(b, c, name='d')
+	e = tf.add(c, const, name='e')
+	a = tf.multiply(d, e, name='a')
 
 # setup the variable initialisation
 init_op = tf.global_variables_initializer()
 
 # start the session
-with tf.Session() as sess:
+with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as sess:
     # initialise the variables
     sess.run(init_op)
     # compute the output of the graph
